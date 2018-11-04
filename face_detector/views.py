27f62249346fb3dde977645daf703a735fa66e55
octavianx <octavianx@stusci.com>
2018-11-04 22:22:32 +0800

    add cascades training, gitignore urls

--- face_detector/views.py
+++ face_detector/views.py
@@ -1,30 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.shortcuts import render
 
 # Create your views here.
 
-# Create your views here.
+
 # import the necessary packages
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import numpy as np
 import urllib
-import urllib.request
 import json
 import cv2
 import os
-import pdb
-
 
 # define the path to the face detector
 FACE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_frontalface_default.xml".format(
 	base_path=os.path.abspath(os.path.dirname(__file__)))
 
-
 @csrf_exempt
 def detect(request):
 	# initialize the data dictionary to be returned by the request
 	data = {"success": False}
-	#data.update({"test":True})
 
 	# check to see if this is a post request
 	if request.method == "POST":
@@ -32,60 +30,56 @@ def detect(request):
 		if request.FILES.get("image", None) is not None:
 			# grab the uploaded image
 			image = _grab_image(stream=request.FILES["image"])
- 
+
 		# otherwise, assume that a URL was passed in
 		else:
 			# grab the URL from the request
 			url = request.POST.get("url", None)
- 
+
 			# if the URL is None, then return an error
 			if url is None:
 				data["error"] = "No URL provided."
 				return JsonResponse(data)
- 
+
 			# load the image and convert
 			image = _grab_image(url=url)
- 
+
 		# convert the image to grayscale, load the face cascade detector,
 		# and detect faces in the image
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)		
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 		detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
-
-		rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5)
-#			minSize=(30, 30), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+		rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5,
+			minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
 		# construct a list of bounding boxes from the detection
 		rects = [(int(x), int(y), int(x + w), int(y + h)) for (x, y, w, h) in rects]
- 
+
 		# update the data dictionary with the faces detected
-		data.update({"num_faces": len(rects), "faces": rects,"success": True})
+		data.update({"num_faces": len(rects), "faces": rects, "success": True})
+
 	# return a JSON response
 	return JsonResponse(data)
- 
+
 def _grab_image(path=None, stream=None, url=None):
 	# if the path is not None, then load the image from disk
 	if path is not None:
 		image = cv2.imread(path)
- 
+
 	# otherwise, the image does not reside on disk
-	else:	
+	else:
 		# if the URL is not None, then download the image
 		if url is not None:
-			#data.update(url)
-			#print url
-			resp = urllib.request.urlopen(url)			
-			data = resp.read()						
-			#return JsonResponse(data)
- 
+			resp = urllib.request.urlopen(url)
+			data = resp.read()
+
 		# if the stream is not None, then the image has been uploaded
 		elif stream is not None:
 			data = stream.read()
- 
+
 		# convert the image to a NumPy array and then read it into
 		# OpenCV format
-		image = np.asarray(bytearray(data), dtype="uint8")		
+		image = np.asarray(bytearray(data), dtype="uint8")
 		image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-		cv2.imwrite('file.jpg',image)
- 
+
 	# return the image
 	return image
